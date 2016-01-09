af1aa8303cb1f4b12a1c7ec9682a90cd2b081758
majetteleger <ma_jetteleger@hotmail.com>
2016-01-09 12:12:30 -0500

    count again

--- count.sh
+++ count.sh
@@ -1,5 +1,6 @@
+# comment
 for filename in data/*.txt
 do
 	echo $filename
-	grep -v Species $filename | cut -d , -f 2 | sort | uniq -c
+	grep -v Species $filename | cut -d , -f 2 | sort | uniq -c 
 done
